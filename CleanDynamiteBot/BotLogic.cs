8fa9522a58bb7154d0b4630c3000a6739ec8a67e
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-17 09:55:15 +0100

    Basic Move Class

--- CleanDynamiteBot/BotLogic.cs
+++ CleanDynamiteBot/BotLogic.cs
@@ -1,14 +1,36 @@
-﻿using System;
-using BotInterface.Bot;
+﻿using BotInterface.Bot;
 using BotInterface.Game;
 
 namespace CleanDynamiteBot
 {
     public class BotLogic : IBot
     {
+        public int MyDynamiteRemaining = 100;
+        public int EnemyDynamiteRemaining = 100;
+        public int RoundsSinceSeenEnemyDynamite = 50;
+        
         public Move MakeMove(Gamestate gamestate)
         {
-            throw new NotImplementedException();
+            Move move = GetMove(gamestate);
+
+            while ((move == Move.D && MyDynamiteRemaining <= 0) ||
+                   (move == Move.W && (EnemyDynamiteRemaining <= 0 || RoundsSinceSeenEnemyDynamite >= 50)))
+            {
+                move = MoveClass.GetRandomMove();
+            }
+            
+            if (move == Move.D)
+            {
+                MyDynamiteRemaining -= 1;
+            }
+
+            return move;
+        }
+
+
+        public Move GetMove(Gamestate gamestate)
+        {
+            return MoveClass.GetRandomMove();
         }
     }
 }
---
+++ CleanDynamiteBot/MoveClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using BotInterface.Game;
+
+namespace CleanDynamiteBot
+{
+    public static class MoveClass
+    {
+        public const int RANDOM_DYNAMITE_WEIGHT = 6;
+        public static Move GetRandomMove()
+        {
+            Random rng = new Random();
+            int randomNum = rng.Next(1000);
+            
+            if (randomNum < RANDOM_DYNAMITE_WEIGHT)
+            {
+                return Move.D;
+            }
+
+            randomNum = rng.Next(900);
+            if (randomNum < 300)
+            {
+                return Move.P;
+            }
+            else if (randomNum < 600)
+            {
+                return Move.R;
+            }
+            return Move.S;
+        }
+        
+        public static Move GetOppositeMove(Move enemyMove)
+        {
+            if (enemyMove == Move.S)
+            {
+                return Move.R;
+            }
+            else if (enemyMove == Move.R)
+            {
+                return Move.P;
+            }
+            else if (enemyMove == Move.P)
+            {
+                return Move.S;
+            }
+
+            return GetRandomMove();
+        }
+        
+        public static Move GetFinisherMove(Move enemyMove)
+        {
+            if (enemyMove == Move.D)
+            {
+                return Move.W;
+            }
+            else if (enemyMove == Move.S || enemyMove == Move.R || enemyMove == Move.P)
+            {
+                return Move.D;
+            }
+            return GetRandomMove();
+        }
+    }
+}
