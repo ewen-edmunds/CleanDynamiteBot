d1fdcf55d0e2fe1bed3106512e7ab30b6a1ef554
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-17 10:04:06 +0100

    DrawLogic

---
+++ CleanDynamiteBot/DrawLogic.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using BotInterface.Game;
+
+namespace CleanDynamiteBot
+{
+    public static class DrawLogic
+    {
+        public static bool WasDrawLastRound(Gamestate gamestate)
+        {
+            Round round = gamestate.GetRounds().Last();
+            return round.GetP1() == round.GetP2();
+        }
+
+        public static bool WasDrawOnRound(Round round)
+        {
+            return round.GetP1() == round.GetP2();
+        }
+
+        public static int GetNumberOfDraws(Gamestate gamestate)
+        {
+            int count = 0;
+            foreach (var round in gamestate.GetRounds())
+            {
+                if (WasDrawOnRound(round)){count += 1;}
+            }
+
+            return count;
+        }
+    }
+}
